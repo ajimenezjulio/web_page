dbe004c4ed81e5bc9a4fdba59e0b506061a147a9
Julio Aguilar Jimenez <jcaj1n17@soton.ac.uk>
2019-01-20 17:46:11 -0600

    ajax mail test 3

--- assets/js/core.js
+++ assets/js/core.js
@@ -590,6 +590,8 @@ var Sukces = {
                             type: 'POST',
                             //url:  'assets/php/contact-form.php',
                             url: 'https://script.google.com/macros/s/AKfycbw9OAX7mt8NeoxZNxYpU21vgPZzW8iyS9escuggEhPjAvRBxhGZ/exec',
+                            dataType: "json",
+                            contentType: 'application/json',
                             data: {'name':'me', 'message':'hi', 'email':'me@me.com'},
                             error       : function(err) { setTimeout(function(){ $btn.addClass('error'); }, 1200); },
                             success     : function(data) {
